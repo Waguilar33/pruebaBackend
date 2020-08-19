2d57e03f927dd138ea97cd8d0642744cd5262837
Will Aguilar <williams.aguilar@geducar.com>
2020-08-19 09:49:58 -0600

    import of crypto module

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const crypto = require("crypto");
 
 // Database
 const loki = require("lokijs");
