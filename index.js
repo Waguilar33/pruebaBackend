800e251285f3b451176df5a60668a75df33300dd
Will Aguilar <williams.aguilar@geducar.com>
2020-08-19 09:17:54 -0600

    Simple server boilerplate

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(3333, () => {
+  console.log("Node server started on port 3333.");
+});
