b8b40ea225c8e00967e433384cbaccf332e294ea
Will Aguilar <williams.aguilar@geducar.com>
2020-08-19 09:18:37 -0600

    Database in memory setup

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
+// Database
+const loki = require("lokijs");
+const db = new loki("database");
+var credentials = db.addCollection("credentials", { indices: ["id"] });
+var messages = db.addCollection("messages", { indices: ["id"] });
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
